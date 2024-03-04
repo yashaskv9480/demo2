73e221b194a4d006476a76bc5a59479c1c30b372
srijith-46 <srijithv.mca22@rvce.edu.in>
2024-03-04 19:59:35 +0530

    Docker

---
+++ LabCycle08/src/main/java/com/example/LabCycle08/BookController.java
@@ -0,0 +1,26 @@
+package com.example.LabCycle08;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+
+public class BookController {
+	private final List<Book> books = new ArrayList<Book>();
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        books.add(book);
+        return book;
+    }
+}
---
+++ LabCycle08/src/main/java/com/example/LabCycle08/LabCycle08Application.java
@@ -0,0 +1,13 @@
+package com.example.LabCycle08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabCycle08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabCycle08Application.class, args);
+	}
+
+}
