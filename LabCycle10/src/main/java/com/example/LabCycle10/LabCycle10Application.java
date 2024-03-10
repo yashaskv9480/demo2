f8763e56ff1c52fa790e5dcc260f3e04a5a28495
srijith-46 <srijithv.mca22@rvce.edu.in>
2024-03-10 10:21:11 +0530

    Kubernetes

---
+++ LabCycle10/src/main/java/com/example/LabCycle10/LabCycle10Application.java
@@ -0,0 +1,13 @@
+package com.example.LabCycle10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabCycle10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabCycle10Application.class, args);
+	}
+
+}
---
+++ LabCycle10/src/test/java/com/example/LabCycle10/LabCycle10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LabCycle10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabCycle10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
